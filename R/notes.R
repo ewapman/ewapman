b3e621c772b5027c2bac33aa31b1fd7380d9a2ac
EWAPMAN <emily.wap@gmail.com>
2020-09-15 11:28:05 -0700

    typo fix

---
+++ R/notes.R
@@ -0,0 +1,59 @@
+### **Education**
+****
+  #### **University of California, Santa Barbara**  
+  - Bachelor of Science, Biology
+- Estimated graduation: June 2022
+- Relevant Coursework: General Chemistry with Labs, Linear Algebra, Physics, Organic Chemistry with Labs, Molecular, Cellular, and Developmental Biology with Labs, Ecology and Evolution, Introduction to Geographic Information Systems
+
+
+### **Experience** 
+****
+  #### **REU Summer Internship: June-July 2020**
+  ##### *Research Assistant|Department of Biology|University of Virginia*
+  - Accepted into the Leadership Alliance research program at the University of Virginia (cancelled due to COVID-19)
+- Attended presentations and recruitment events hosted by the Leadership Alliance Virtual Professional Development program
+
+#### **Young Lab: October 2019-Present** 
+##### *Research Assistant|Ecology, Evolution, and Marine Biology|UC Santa Barbara*
+- Classified, organized, and sorted various freshwater insects collected at alpine lakes of varying elevation
+Goleta, CA
+- Counted and recorded the number and order of insects in each sample to use for future population analysis
+
+
+#### **Marcu Laboratory: July 2019-August 2019** 
+##### *Research Assistant|Biomedical Engineering|UC Davis* 
+- Created phantoms to use as a baseline for testing Fluorescence Lifetime Imaging (FLIm) machines using a silicone base and dyes, problem solved experimental issues, and tested the lifetimes and intensities of these phantoms using FLIm technology
+- Analyzed images and data relating to fluorescence lifetime and intensity from FLIm scanning using MATLAB and Excel, and wrote a Standard Operating Procedure of the process
+- Presented project and experimental results to other lab members
+- Attended weekly lab and journal club meetings and read scientific papers relating to current experiments
+- Learned cell culture practices, sterile techniques, and pipetting
+
+
+#### **Santa Barbara Wildlife Care Network: April 2019-June 2019** 
+##### *Volunteer|Goleta, CA*
+- Performed maintenance tasks to keep animal enclosures sterile and clean, and organized equipment and medical materials
+- Provided care for wounded and sick animals and assisted veterinary procedures
+
+
+#### **Robert Livermore Aquatics Center: June 2017 – September 2019** 
+##### *Lifeguard/Swim Instructor (Seasonal)|Livermore, CA* 
+- Developed leadership skills by enforcing facility regulations and practiced life-saving scenarios at staff trainings
+- Created swim lesson plans and taught swimming technique to groups of children and adults of all ages and abilities
+
+
+### **Skills and Awards**
+****
+  - Dean’s Honors (3 quarters)
+- Pedrozzi Scholarship
+- Proficient in Spanish
+- GIS in R 
+
+
+### **Activities**
+****
+  #### **UCSB Triathlon Team: October 2018-Present**
+  ##### *Kendra’s Race Committee Officer* 
+  - Met weekly with other committee members to plan the team’s annual spring race
+- Helped organize course set-up and volunteers for race day
+
+
